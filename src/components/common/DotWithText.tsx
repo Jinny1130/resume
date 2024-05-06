4716333e024f63143fe4b7e18c9cb5ff8aea272f
choiwoojin <ggogo33@daum.net>
2024-05-06 23:23:16 +0900

    [MODIFY] work experience 페이지별 설명 부분 추가

--- src/components/common/DotWithText.tsx
+++ src/components/common/DotWithText.tsx
@@ -9,7 +9,7 @@ const DotWithText:React.FC<OwnProps> = ( { sideText, textSize } ) => {
     return (
         <div className={`w-full py-1 text-${textSize}`}>
             <p className="flex items-baseline">
-                <i className="block w-1 h-1 rounded-full bg-sky-400 mr-1"/>{ sideText }
+                <i className={`block w-1 h-1 rounded-full bg-sky-400 ${textSize === 'base' ? 'mr-2' : 'mr-1'}`}/>{ sideText }
             </p>
         </div>
     )
