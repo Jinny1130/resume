670ac40c6b1dbbc3be845a807228a9eeaec09d8c
choiwoojin <ggogo33@paymint.co.kr>
2024-05-07 16:32:42 +0900

    [MODIFY] header useEffect > page 파일로 이동, eventListener 'scroll' 'resize' 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import "@/styles/resume.scss";
 import Header from '@/components/common/Header';
 import NameWrap from "@/components/section/NameWrap";
@@ -7,12 +9,44 @@ import Career from "@/components/section/Career";
 import WorkExperience from "@/components/section/WorkExperience";
 import SideProject from "@/components/section/SideProject";
 import Footer from '@/components/common/Footer';
+import { useEffect, useState } from "react"
 
 export default function Home() {
+    const [isMobile, setIsMobile] = useState('pc');
+
+    useEffect( () => {
+        
+        function setHeaderShadow() {
+            if (window.scrollY === 0) {
+                document.querySelector('#header')?.classList.remove('shadow')
+            }
+            else {
+                document.querySelector('#header')?.classList.add('shadow')
+            }
+        }
+
+        function handleResize() {
+            setIsMobile(window.innerWidth < 640 ? 'mo' : 'pc'); // 640px 미만은 모바일로 간주
+        }
+
+        // 초기 width 사이즈 확인
+        handleResize();
+
+        window.addEventListener('scroll', setHeaderShadow);
+        window.addEventListener('resize', handleResize);
+
+        // 언마운트 시 리스너 제거
+        return () => {
+            window.removeEventListener('scroll', setHeaderShadow);
+            window.removeEventListener('resize', handleResize);
+        }
+
+    }, [])
+
     return (
         <div>
             <Header />
-            
+
             <div className="resume-wrap max-w-4xl pt-24 px-6 mx-auto sm:px-3">
                 <NameWrap />
                 <ContactAndSkill />
@@ -22,7 +56,7 @@ export default function Home() {
                 <SideProject />
             </div>
 
-            <Footer />
+            <Footer isMobile={isMobile} />
         </div>
     );
 }
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,22 +1,7 @@
-'use client'
-
 import Image from "next/image"
 import Link from "next/link"
-import { useEffect } from "react"
 
 const Header = () => {
-
-    useEffect( () => {
-        document.addEventListener('scroll', (e) => {
-            if (window.scrollY === 0) {
-                document.querySelector('#header')?.classList.remove('shadow')
-            }
-            else {
-                document.querySelector('#header')?.classList.add('shadow')
-            }
-        })
-    })
-
     return (
         <div id="header" className="header w-full h-14 flex items-center justify-between px-3 fixed bg-white z-10">
             <Link href={'/'} className="h-full py-2.5 flex items-center font-bold">
